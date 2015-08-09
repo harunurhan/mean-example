ca2b1c205cb1a9119e295e1046592637dd6c1e2c
harunurhan <harunurhan17@gmail.com>
2015-08-09 19:38:27 +0200

    Add tests for POST, GET:id and DELETE:id

--- test/movies.js
+++ test/movies.js
@@ -1,19 +1,69 @@
 const should = require('should');
 const request = require('supertest');
 const config = require('../config');
+const url = 'http://localhost:8000';
 describe('Routing', function(){
+
 	describe('Movies', function() {
 		it('should GET correctly', function(done) {
-			request('http://localhost:8000')
+			request(url)
 			   	.get('/api/movies')
 				.expect(200)
 		 		.end(function(err, res) {
 					if (err) {
 						return done(err);
 					}
+					//TODO: check if response array is empty or not
 					done();
 			});		
 		});
+
+		var _id;
+		it('should POST new movie', function(done) {
+			var movie = {title: 'Mocha Test'
+						,releaseYear: '2015'
+						,director: 'Harun'
+						,genre: 'Testing'
+						,language: 'Javascript'};
+			request(url)
+				.post('/api/movies')
+				.send(movie)
+				.expect(200)
+				.end(function(err, res) {
+					if(err) {
+						return done(err);
+					}
+					should.exist(res.body._id, 'POST response include _id');
+					_id = res.body._id;
+					done();
+				});
+		});
+		
+		it('should GET by id', function(done) {
+			request(url)
+				.get('/api/movies/'+_id)
+				.expect(200)
+				.end(function(err, res) {
+					if(err) {
+						return done(err);
+					}
+					done();
+			});
+		});
+		
+		it('should DELETE by id', function(done) {
+			request(url)
+				.delete('/api/movies/'+_id)
+				.expect(200)
+				.end(function(err, res) {
+					if(err) {
+						return done(err);
+					}
+					should.exist(res.body.message, 'DELETE response include message');
+					done();	
+			});
+		});
+
 	});
 
 });
